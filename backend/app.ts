0650f78729a7e4373316bda809908a38a8f5710d
tuancamtbtx <nguyenvantuan140397@gmail.com>
2024-05-26 20:05:08 +0700

    first commit

---
+++ backend/app.ts
@@ -0,0 +1,30 @@
+import express, { Application } from "express";
+import sequelize from "@src/sequelize";
+
+import CustomerRouter from "@src/routes/CustomerRouter";
+import logger from "@src/utils/logger";
+import { errorHandler } from "@src/middlewares/ErrorMiddleware";
+
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+async function initialize() {
+  try {
+    await sequelize.sync({ force: true }); // Sync the models with the database
+  } catch (error) {
+    console.error("Error initializing Sequelize:", error);
+  }
+}
+initialize().then(() => {
+  console.log("Initialized Sequelize");
+});
+app.use("/api/customers", CustomerRouter);
+app.use(errorHandler);
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
+});
---
+++ backend/src/configs/DBConfig.ts
@@ -0,0 +1,17 @@
+require("dotenv").config();
+process.env.PORT;
+
+console.log(process.env);
+export default {
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
