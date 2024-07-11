7f9018b92a021424e3ef4544d03febf85380c3e0
tuancamtbtx <nguyenvantuan140397@gmail.com>
2024-07-11 18:03:36 +0700

    [add] authen for app

---
+++ frontend/src/app/[...nextauth]/options.ts
@@ -0,0 +1,13 @@
+// 'use client'
+import type { NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const options: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET as string
+}
